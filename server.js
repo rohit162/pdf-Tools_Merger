3391f404edc7fa28f73a45eb3051c26e1e8e2334
unknown <rohitmittal1620@gmail.com>
2023-10-03 14:13:29 +0530

    pdfx merger tool

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
+
+const {mergedPdfs}=require('./merge.js')
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"templates/index.html"))
+})
+
+app.post('/merge', upload.array('pdfs', 12), async (req, res, next)=> {
+    console.log(req.files)
+   const d= await mergedPdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    // res.send({data :req.files})
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
